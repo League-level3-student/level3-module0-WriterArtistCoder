e30f7e04ec59ba8cade4d749908b1c15ef801656
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-17 12:57:52 -0700

    _01_RobotRace, _02_FindTheHiddenButton

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -15,12 +15,11 @@ public class _01_RobotRace {
 		straightRace(20);
 		straightRace(44);
 		// 9. make the robots race around a circular track.
-
+		circularRace(20);
 	}
 	
 	public static void straightRace(int robots) {
 		Robot[] bonsai = new Robot[robots];
-		new Robot
 		// 3. use a for loop to initialize the robots.
 		for (int i = 0; i < bonsai.length; i++) {
 			// 4. make each robot start at the bottom of the screen, side by side, facing up
@@ -49,5 +48,54 @@ public class _01_RobotRace {
 		// 7. declare that robot the winner and throw it a party!
 		JOptionPane.showMessageDialog(null, "Kudos to RACERBOT #AX" + winner
 				+ "DJZ! They won and let's give\n them a big round of applause! *long silence*");
+		for (Robot r : bonsai) {
+			r.hide();
+			r.unSparkle();
+		}
+	}
+	
+	public static void circularRace(int robots) {
+		Robot[] bonsai = new Robot[robots];
+		int[] turned = new int[robots];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < bonsai.length; i++) {
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			bonsai[i] = new Robot();
+			bonsai[i].miniaturize();
+			bonsai[i].moveTo(((i + 1) * 20), 520);
+		}
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		int winner = -1;
+		while (winner == -1) {
+			for (int i = 0; i < bonsai.length; i++) {
+				bonsai[i].setSpeed(100);
+				bonsai[i].move(new Random().nextInt(50));
+
+				if (bonsai[i].getY() <= 100 && bonsai[i].getX() < 500 && turned[i] == 0
+						|| bonsai[i].getY() <= 100 && bonsai[i].getX() >= 500 && turned[i] == 1
+						|| bonsai[i].getY() >= 500 && bonsai[i].getX() >= 500 && turned[i] == 2
+|| bonsai[i].getY() >= 500 && bonsai[i].getX() <= 100 && turned[i] == 3) {
+					bonsai[i].turn(90);
+					turned[i]++;
+				}
+				
+				if (turned[i] == 4) {
+					bonsai[i].sparkle();
+					winner = i;
+					break;
+				}
+			}
+		}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+
+		// 7. declare that robot the winner and throw it a party!
+		JOptionPane.showMessageDialog(null, "Kudos to RACERBOT #AX" + winner
+				+ "DJZ! They won and let's give\n them a big round of applause! *long silence*");
+		for (Robot r : bonsai) {
+			r.hide();
+			r.unSparkle();
+		}
 	}
 }
--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -16,9 +16,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] alexLovesSpiders;
 	//2 create an integer variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -29,30 +29,46 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		int alexLovesUranium = 10;
+		try {
+			alexLovesUranium = Integer.parseInt(JOptionPane.showInputDialog("Enter a number greater than 0."));
+			if (alexLovesUranium <= 0) {
+				alexLovesUranium = 10;
+			}
+		} catch (Exception e) {
+			
+		}
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		alexLovesSpiders = new JButton[alexLovesUranium];
 		//5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < alexLovesUranium; i++) {
 			//6. initialize each JButton in the array
+			alexLovesSpiders[i] = new JButton();
 			//7. add the ActionListener to each JButton
+			alexLovesSpiders[i].addActionListener(this);
 			//8. add each JButton to the panel
-		
+			panel.add(alexLovesSpiders[i]);
+		}
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Here are the instructions:\n Click the button that\n flashes to WINNNNNNNN!");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
+		hiddenButton = new Random().nextInt(alexLovesUranium);
 		//14. Set the text of the JButton located at hiddenButton to read "ME"
-
+		alexLovesSpiders[hiddenButton].setText("ME");
 		//15. Use Thread.sleep(100); to pause the program.
-		
+		try {
+			Thread.sleep(100);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+		alexLovesSpiders[hiddenButton].setText("");
 	}
 
 	@Override
@@ -60,7 +76,11 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-
+		if (buttonClicked.equals(alexLovesSpiders[hiddenButton])) {
+			JOptionPane.showMessageDialog(null, "Computer: Hey!\n You: Hi.\n Computer: You win!\n You: Cool!\n Computer: How dare you not be more excited! I will unleash my evil -\n You: [shuts down computer]\n Computer: [silence]\n Orangutan: Huh.\n You: AH!");
+		} else {
 		//18. else tell them to try again
+			JOptionPane.showMessageDialog(null, "Computer: Try again!\n You: [sobs with despair]\n Computer: Cheer up! Wanna hear a knock-knock joke?\n You: [sniff] Okay...\n Computer: Boo.\n You: Ah! You scared me!\n Computer: That's not how it goes. Say \"Boo who.\"\n You: Boo who?\n Computer: Don't cry, it's just a joke!\n You: AH! A BAD PUN! MY WEAKNESS! [hissss...]\n Computer: Stop deflating.");
+		}
 	}
 }
